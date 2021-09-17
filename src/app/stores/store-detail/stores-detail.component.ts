c7530166e0f6590abb63b67c27101eb05b3c362c
DESKTOP-4QHTB27\sambu <sambutler1017@icloud.com>
2021-09-17 11:28:06 -0400

    no message

--- src/app/stores/store-detail/stores-detail.component.ts
+++ src/app/stores/store-detail/stores-detail.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router';
 import { default as json } from 'projects/insite-kit/src/lib/assets/translations/stores/en.json';
 import { Store } from 'projects/insite-kit/src/lib/models/store.model';
 import { User } from 'projects/insite-kit/src/lib/models/user.model';
+import { iff, of } from 'rxjs';
 import { map, switchMap, tap } from 'rxjs/operators';
 import { StoreService } from 'src/service/store-service/store-service.service';
 import { UserService } from 'src/service/user-service/user.service';
@@ -17,6 +18,7 @@ export class StoresDetailComponent implements OnInit {
   managerInfo: User;
   storeJson = json;
   managers = [];
+  loading = true;
 
   constructor(
     private activeRoute: ActivatedRoute,
@@ -25,6 +27,7 @@ export class StoresDetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+    this.loading = true;
     const params: any = this.activeRoute.params;
     this.storeService
       .getStores(new Map<string, string>().set('id', params.value.id))
@@ -33,8 +36,11 @@ export class StoresDetailComponent implements OnInit {
         tap((res) => (this.storeInfo = res)),
         switchMap(() => this.userService.getUserById(this.storeInfo.regionalId)),
         tap(res => this.regionalInfo = res),
-        switchMap(() => this.userService.getUserById(this.storeInfo.managerId)),
-      )
-      .subscribe((res) => (this.managerInfo = res));
+        switchMap(() => iff(() => !!this.storeInfo.managerId, this.userService.getUserById(this.storeInfo.managerId), of(null)),
+        )
+          .subscribe((res) => {
+            this.loading = false;
+            this.managerInfo = res;
+          });
   }
 }
