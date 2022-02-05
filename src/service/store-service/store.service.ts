27fcfd89f4ff28d2651c97f755f0be15f3c0ba70
Samuel Butler <sambutler1017@icloud.com>
2022-02-05 12:00:16 -0500

    no message

--- src/service/store-service/store.service.ts
+++ src/service/store-service/store.service.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class StoreService {
+  readonly BASE_STORE_PATH = 'api/store-app/stores';
+
   constructor(private readonly requestService: RequestService) {}
 
   /**
@@ -16,6 +18,35 @@ export class StoreService {
    * @returns observable of the returned request
    */
   getStores(params?: Map<string, string[]>): Observable<Store[]> {
-    return this.requestService.get<Store[]>('api/store-app/stores', params);
+    return this.requestService.get<Store[]>(this.BASE_STORE_PATH, params);
+  }
+
+  /**
+   * This will update the store manager of a store for the given user id and store id.
+   *
+   * @param userId The user id to update the store manager too.
+   * @param storeId The store to update the manager at.
+   * @returns store object with the updated manager.
+   */
+  updateStoreManagerOfStore(
+    userId: number,
+    storeId: string
+  ): Observable<Store> {
+    return this.requestService.put<Store>(
+      `${this.BASE_STORE_PATH}/${userId}/manager/${storeId}`
+    );
+  }
+
+  /**
+   * This will update the regional of a store for the given user id and store id.
+   *
+   * @param userId The user id to update the regional too.
+   * @param storeId The store to update the regional at.
+   * @returns store object with the updated regional.
+   */
+  updateRegionalOfStore(userId: number, storeId: string): Observable<Store> {
+    return this.requestService.put<Store>(
+      `${this.BASE_STORE_PATH}/${userId}/regional/${storeId}`
+    );
   }
 }
