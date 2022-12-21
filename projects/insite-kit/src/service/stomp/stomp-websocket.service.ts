5ab42b08d23e82de4563bd74b219f38b5aaed552
Samuel Butler <sambutler1017@icloud.com>
2022-12-20 22:34:36 -0500

    no message

--- projects/insite-kit/src/service/stomp/stomp-websocket.service.ts
+++ projects/insite-kit/src/service/stomp/stomp-websocket.service.ts
@@ -3,8 +3,7 @@ import { RxStomp } from '@stomp/rx-stomp';
 import { Message } from '@stomp/stompjs';
 import { Notification } from 'projects/insite-kit/src/models/notification.model';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { User } from '../../models/user.model';
+import { map, switchMap } from 'rxjs/operators';
 import { JwtService } from '../jwt-service/jwt.service';
 import { UrlService } from '../url-service/url.service';
 import { STOMP_SOCKET_CONFIG } from './stomp.config';
@@ -13,9 +12,7 @@ import { STOMP_SOCKET_CONFIG } from './stomp.config';
   providedIn: 'root',
 })
 export class StompWebSocketService extends RxStomp {
-  private readonly SOCKET_URL = '/topic/notifications';
-
-  activeUser: User;
+  private readonly SOCKET_URL = '/queue/user/notification';
 
   constructor(
     private readonly jwt: JwtService,
@@ -24,6 +21,10 @@ export class StompWebSocketService extends RxStomp {
     super();
   }
 
+  /**
+   * Initialize the socket connection. This will only initialize if the connection
+   * is not already established.
+   */
   init() {
     if (!this.active) {
       this.configure(STOMP_SOCKET_CONFIG);
@@ -35,19 +36,50 @@ export class StompWebSocketService extends RxStomp {
   }
 
   /**
-   * This will listen to the websocket url for any request and rely
-   * them to the logged in user.
+   * Kill the websocket connection.
+   */
+  disconnect() {
+    this.deactivate();
+  }
+
+  /**
+   * This will listen to the websocket url for any request and show it to the
+   * provided user.
    *
-   * @param user The user currently logged in
+   * @param destination What socket path to listen too.
+   * @param userSession Determines if the connection is unique to the user.
    * @returns Observable of the caught Notification object.
    */
-  listen(destination?: string): Observable<Notification> {
-    return super
-      .watch(
-        `${
-          destination ? destination : this.SOCKET_URL
-        }/${this.jwt.getRequiredUserId()}`
+  listen(): Observable<Notification> {
+    return this.subscriptionSession().pipe(
+      switchMap((session) =>
+        super
+          .watch(this.buildSocketPath(this.SOCKET_URL, session))
+          .pipe(map((res: Message) => JSON.parse(res.body)))
       )
-      .pipe(map((res: Message) => JSON.parse(res.body)));
+    );
+  }
+
+  /**
+   * Builds out the socket path for the subscription. If the connection desires that it
+   * be user specific then it will append the users unique sesion id to the
+   * subscription call.
+   *
+   * @param des Where the subscription should take place.
+   * @param ses The unique user session id.
+   * @returns String of the built socket path.
+   */
+  buildSocketPath(des: string, uuid: string): string {
+    return `${des}-${uuid}`;
+  }
+
+  /**
+   * Gets the session id for the user. This will be a UUID that will
+   * only belong to this user logged in.
+   *
+   * @returns Observable of the session id.
+   */
+  subscriptionSession() {
+    return this.serverHeaders$.pipe(map((session) => session['user-name']));
   }
 }
